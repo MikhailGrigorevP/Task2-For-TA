61c9fd750749ca3855f09fc02296ac6fba1f270c
Mikhail <16112000m@gmail.com>
2020-04-22 23:30:19 +0300

    - path finder beta

--- Tests/Path.py
+++ Tests/Path.py
@@ -12,18 +12,32 @@ map:
     #####      ####
     ###############
 
-    # РОБОТ СМОТРИТ ВПРАВО
+    function of integer return_y (stack_y, size_y, y)
+        return y + stack_y[size_y-1] - stack_y[size_y-2]
+
+    function of integer return_x (stack_x, size_x, x)
+        return x + stack_x[size_x-1] - stack_x[size_x-2]
 
     function of integer findPath(x, y) begin
-
+        integer turn := 0
+        boolean exit := false
         do begin
             if reflect = "EXIT" then exit = true
             else begin
                 if forward <> true then begin
-                    x := x + 1
-                    end else if left <> true then
+                    # TODO EXPAND MAP
+                    integer i := lms
+                    # TODO STEPS
+                end else begin
+                    right
+                    turn := turn + 1
+                if turn = 4 then begin
+                    turn := 0
+                    x := stack_x pop back
+                    y := stack_y pop back
                 end
             end until exit <> true
+
 
         end
 
