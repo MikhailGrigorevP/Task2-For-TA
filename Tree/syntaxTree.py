9dd755a4b0d32de29274afd6b4a03bc9750dafa9
Mikhail <16112000m@gmail.com>
2020-04-25 18:18:30 +0300

    - Update tests

--- Tree/syntaxTree.py
+++ Tree/syntaxTree.py
@@ -19,22 +19,16 @@ class node(object):
     # print Tree
     def print(self, lvl=0):
         # offset
-        print(' ' * lvl, ' ', self)
-        if self.child is not None:
-            if isinstance(self.child, node):
-                self.child.print(lvl + 1)
-            elif isinstance(self.child, str):
-                print(' ' * (lvl + 1), self.child)
-            elif isinstance(self.child, list):
-                for i in range(len(self.child)):
-                    if isinstance(self.child[i], str):
-                        print(' ' * (lvl + 1), self.child[i])
-                    else:
-                        self.child[i].print(lvl + 1)
-            elif isinstance(self.child, dict):
-                for key, value in self.child.items():
-                    print(' ' * (lvl + 1), key)
-                    if isinstance(value, str):
-                        print(' ' * (lvl + 2), value)
-                    elif isinstance(value, node):
-                        value.print(lvl + 2)
+        if self is None:
+            return
+        print(' ' * lvl, self)
+        if isinstance(self.child, list):
+            for child in self.child:
+                child.print(lvl + 1)
+        elif isinstance(self.child, node):
+            self.child.print(lvl + 1)
+        elif isinstance(self.child, dict):
+            for key, value in self.child.items():
+                print(' ' * (lvl + 1), key)
+                if value:
+                    value.print(lvl + 2)
